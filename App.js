7dc46e2b24f8f8cf84f41c37718d989c5a6ed5a0
MasatoHayakawa <MasatoHayakawa@172-3-11-10.lightspeed.miamfl.sbcglobal.net>
2016-04-21 14:46:01 +0900

    lifecycle

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { val: 0 }
+    this.update = this.update.bind(this)
+  }
+  update(e) {
+    this.setState({ val: this.state.val + 1 })
+  }
+  componentWillMount() {
+    console.log("mounting")
+  }
   render() {
-    return <Button>I <Heart/> React</Button>
+    console.log("rendering!")
+    return (
+      <button onClick={this.update}>{this.state.val}</button>
+    )
+  }
+  componentDidMount() {
+    console.log("mountied")
   }
 }
-
-class Button extends React.Component {
-  render() {
-    return <button>{this.props.children}</button>
-  }
-}
-
-const Heart = () => <span>♥</span>
 
 export default App
