8325a956b41145a09bc4ae5992fa64b247073b35
MasatoHayakawa <MasatoHayakawa@172-3-11-10.lightspeed.miamfl.sbcglobal.net>
2016-04-21 14:03:43 +0900

    pass vars

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>Hello</h1>
-        <b>world</b>
-      </div>
-    ) 
+    let txt = this.props.txt
+    return <h1>{txt}</h1>
   }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  num: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "DEFAULT"
 }
 
 export default App
