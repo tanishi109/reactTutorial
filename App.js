eb2e8c5e9941bc29b44543cdbd5c84efd5e86714
MasatoHayakawa <MasatoHayakawa@172-3-11-10.lightspeed.miamfl.sbcglobal.net>
2016-04-21 13:53:36 +0900

    another one

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-class App extends React.Component {
-  render() {
-    return <div>Hello</div>
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return <div>Hello</div>
+//   }
+// }
+
+const App = () => <h1>Hi</h1>
 
 export default App
