02f3b1ec328a31af97368b264bb39e06b09dded1
MasatoHayakawa <MasatoHayakawa@172-3-11-10.lightspeed.miamfl.sbcglobal.net>
2016-04-21 13:55:59 +0900

    multi dom

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-// class App extends React.Component {
-//   render() {
-//     return <div>Hello</div>
-//   }
-// }
 
-const App = () => <h1>Hi</h1>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <b>world</b>
+      </div>
+    ) 
+  }
+}
 
 export default App
