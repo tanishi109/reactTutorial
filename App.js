8f3b08cc01bd1b0ac38cb5d57505499f39bd27fe
MasatoHayakawa <MasatoHayakawa@172-3-11-10.lightspeed.miamfl.sbcglobal.net>
2016-04-21 14:13:23 +0900

    Widget compornet

--- App.js
+++ App.js
@@ -16,11 +16,19 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" onChange={this.update} />
-        <h1>{this.state.txt}</h1>
+        <Widget txt={this.state.txt} update={this.update} />
       </div>
     )
   }
+}
+
+const Widget = (props) => {
+    return (
+      <div>
+        <input type="text" onChange={props.update} />
+        <h1>{props.txt}</h1>
+      </div>
+    )
 }
 
 export default App
